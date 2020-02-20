8c1d8e0ac39bdea0e757199f6ebdff16c50093bc
shenzhenghuan <shenzhenghuan@styd.cn>
2020-02-20 18:20:31 +0800

    first commit to local rep

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName Demo
+ * @Description TDD
+ * @Author shenzhenghuan
+ * @Date 2020/2/20 15:17
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
