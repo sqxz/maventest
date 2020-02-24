b948e96c50891907f0658c425a30400ff6370207
shenzhenghuan <shenzhenghuan@styd.cn>
2020-02-24 19:00:55 +0800

    反射调用方法

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,6 +2,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @ClassName Demo
@@ -13,7 +15,7 @@ public class Demo {
 
     final static Logger logger = LogManager.getLogger(Demo.class);//用final修饰field可以阻止被重新赋值
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Demo demo = new Demo();
         int a = demo.getTax(new SalaryTax(),1000);
         int b = demo.getTax(new OtherTax(),1000);
@@ -35,13 +37,22 @@ public class Demo {
         logger.info(test1);
     }
 
-    public static void  reflectionGetFiled() throws NoSuchFieldException, IllegalAccessException {
-        Student student = new Student("shen","111111");
+    public static void  reflectionGetFiled() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Student student = new Student("shen","111111",99);
         Field field = Student.class.getDeclaredField("name");
         field.setAccessible(true);
         field.get(student);
         logger.info(field);
         logger.info(field.get(student));
+        Method method = student.getClass().getDeclaredMethod("getScore");
+        logger.info(method);
+        Method method1 = student.getClass().getDeclaredMethod("getLevel",String.class); //获取有参数的方法
+        logger.info(method1);
+        String level = (String) method1.invoke(student,"优秀"); //用反射的方法调用方法
+        logger.info(level);
+        Method method2 = student.getClass().getDeclaredMethod("getGrade");
+        String grade = (String) method2.invoke(null,null);//调用静态方法时，由于无需指定实例对象，所以invoke方法传入的第一个参数永远为null
+        logger.info(grade);
     }
 
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,9 +2,26 @@
 public class Student {
     private String name;
     private String password;
+    private int score;
 
-    public Student(String name,String password){
+    public Student(String name,String password,int score){
         this.name = name;
         this.password = password;
+        this.score = score;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public String getLevel(String title){
+        if (title.equals("优秀")){
+            return "A";
+        }else
+            return "B";
+    }
+
+    public static String getGrade(){
+        return "一年级";
     }
 }
