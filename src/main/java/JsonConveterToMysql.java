4939ac8879b07a244504091edff1ff89a0c71194
shenzhenghuan <shenzhenghuan@styd.cn>
2021-02-18 17:45:11 +0800

    优化

--- src/main/java/JsonConveterToMysql.java
+++ src/main/java/JsonConveterToMysql.java
@@ -2,6 +2,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -49,6 +50,28 @@ public class JsonConveterToMysql {
     }
 
 
+    public String insertValueByJson(String json,String tableName){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("insert into "+ tableName +"(id,");
+        Gson gson= new Gson();
+        JsonObject jsonObject = gson.fromJson(json,JsonObject.class);
+        Set<Map.Entry<String, JsonElement>> set = jsonObject.entrySet();
+        for (Map.Entry<String, JsonElement> entry : set) {
+            stringBuilder.append(entry.getKey()+",");
+        }
+        stringBuilder.append("comment,expected) VALUES(1,");
+        for (Map.Entry<String, JsonElement> entry : set) {
+            if (entry.getValue().isJsonArray() || entry.getValue().toString().indexOf("{") == 0){
+                stringBuilder.append("\'"+entry.getValue()+"\'"+",");
+            }else {
+                stringBuilder.append(entry.getValue()+",");
+            }
+        }
+
+        stringBuilder.append("null,null)");
+        return stringBuilder.toString();
+    }
+
 
 
 
@@ -56,5 +79,7 @@ public class JsonConveterToMysql {
         JsonConveterToMysql jsonConveterToMysql = new JsonConveterToMysql();
         String json = "{\"card_name\":\"储值卡0203\",\"special_note\":\"预约购课更方便更优惠\",\"card_price\":\"100\",\"sell_price\":\"10\",\"num\":\"10\",\"unit\":1,\"card_consumer_id\":[1,2,3,4,5,6,7,8],\"consumption_range\":1,\"consumer_shop_list\":[],\"support_sales\":1,\"sell_shop_list\":[],\"start_time\":\"2021-02-03\",\"end_time\":\"2021-02-28\",\"endOpen\":false,\"bg_image\":{\"color\":\"#514847\",\"image_id\":0,\"image_key\":\"image/default/bg-card-selection-1.png\",\"image_url\":\"http://styd-saas-public.oss-cn-shanghai.aliyuncs.com/image/default/bg-card-selection-1.png\",\"index\":1},\"appConfig\":true,\"card_contents\":\"\",\"description\":\"\",\"_is_transfer\":true,\"is_transfer\":1,\"transfer_unit\":2,\"transfer_num\":4,\"card_sell_type\":[2,1]}";
         System.out.println(jsonConveterToMysql.ConvteToMysql(json));
+        System.out.println(jsonConveterToMysql.insertValueByJson(json,"createDepositCard"));
+
     }
 }
